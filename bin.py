afaeec870b9637eff808a3d94dc5031f289903ef
Felimartinez1 <127166894+Felimartinez1@users.noreply.github.com>
2023-03-11 17:30:33 -0300

    Delete bin.py

--- bin.py
+++
@@ -1,8 +0,0 @@
-def binary(decimal):
-    binary = ''
-    while decimal // 2 != 0:
-        binary = str(decimal % 2) + binary
-        decimal = decimal // 2
-    return str(decimal) + binary
-number = int(input('enter the number that you want to convert to binary: '))
-print (binary(number))
